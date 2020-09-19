b8259a9fe1a244b7e68ace71975d856095ebed46
igor <igor@iwt.com.br>
2020-09-18 23:32:05 -0300

    Updates

---
+++ precache-manifest.ace5b026074a12cf0392f2d219fcd2c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa36cfc1bc4989432c54edd70d59cba",
+    "url": "/projeto-web/index.html"
+  },
+  {
+    "revision": "d3860bb0be26e9ad91ce",
+    "url": "/projeto-web/static/css/main.d5249fb1.chunk.css"
+  },
+  {
+    "revision": "bd740e1ca9d2dbfde46d",
+    "url": "/projeto-web/static/js/2.b82769e2.chunk.js"
+  },
+  {
+    "revision": "0aadc8a8405aeed76ae7e020b1d921e7",
+    "url": "/projeto-web/static/js/2.b82769e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3860bb0be26e9ad91ce",
+    "url": "/projeto-web/static/js/main.e91bb654.chunk.js"
+  },
+  {
+    "revision": "e010aae546d11e15f1dc",
+    "url": "/projeto-web/static/js/runtime-main.0e382619.js"
+  },
+  {
+    "revision": "13745892fe019f0b81c969c78c78de1d",
+    "url": "/projeto-web/static/media/logo.13745892.svg"
+  }
+]);
